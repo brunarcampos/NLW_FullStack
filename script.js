b10df916b4382a2aff9b3c9e7a651797d2ad0efb
Bruna Campos <brunareginadecampos@gmail.com>
2025-07-10 13:29:04 -0300

    tiraran a senha

--- script.js
+++ script.js
@@ -10,7 +10,6 @@ const markdownToHTML = (text) => {
     return converter.makeHtml(text)
 }
 
-//AIzaSyDIbJ9ULMWopAvSY3_7EqoKiRL8vmN24Ms
 const perguntarAI = async (question, game, apiKey) => {
     const model = "gemini-2.5-flash"
     const geminiURL = `https://generativelanguage.googleapis.com/v1beta/models/${model}:generateContent?key=${apiKey}`
