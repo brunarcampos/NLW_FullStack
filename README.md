01806f1b97b2165ad2e43d724b181529e8d1d17a
Bruna Campos <brunareginadecampos@gmail.com>
2025-07-10 13:21:46 -0300

    Projeto finalizado

--- README.md
+++ README.md
@@ -24,7 +24,9 @@ Um evento onde colocamos em prática nossos conhecimentos em HTML,CSS,JavaScript
     - Tranferência
 - Protocol
     - Protocolo
-    - COnjunto de regras
+    - Conjunto de regras
+- Methods HTTP (verbos): Get, Post, Patch/Put, Delete
+- Headers (cabeçalhos): Instruções/Informações para cada chamada
 
 # URL
 - Uniform
@@ -104,5 +106,36 @@ A partir disso, o HTML é impactado visualmente.
 - Document
 - Object
 - Model
+
+# API
+- Application
+- Programming
+- Interface
+- JSON: JavaScript Object Notation
+
+# CDN
+- Content
+- Delivery
+- Network
+
+# LLM
+- Large
+- Language
+- Model
+- Gemini, OpenAi (chatGPT), Anthropic (Claude)...
+
+# Agentes AI
+- Tools (ferramentas): permitem uso de código, ou apps, ou qualquer informação extra.
+- Melhor contexto
+
+# Engenharia de prompt
+- One Shot: uma única pergunta sem muito contexto
+- Few shot: é apresentado exemplos do que se espera
+- Chain of Thought: Cadeia de pensamento para a IA responder gradativamente conforme a instrução.
+
+
+
+
+
 
 
--- script.js
+++ script.js
@@ -5,8 +5,96 @@ const askButton = document.getElementById('askButton')
 const aiResponse = document.getElementById('aiResponse')
 const form = document.getElementById('form')
 
-const enviarFormulario = (event) => {
+const markdownToHTML = (text) => {
+    const converter = new showdown.Converter()
+    return converter.makeHtml(text)
+}
+
+//AIzaSyDIbJ9ULMWopAvSY3_7EqoKiRL8vmN24Ms
+const perguntarAI = async (question, game, apiKey) => {
+    const model = "gemini-2.5-flash"
+    const geminiURL = `https://generativelanguage.googleapis.com/v1beta/models/${model}:generateContent?key=${apiKey}`
+    const pergunta = `
+        ## Especialidade
+        Você é um especialista assistente de mate para o jogo ${game}
+
+        ## Tarefa
+        Você deve responder as perguntas so usuário com base no seu conhecimento do jogo, estratégias, build e dicas
+
+        ## Regras
+        - Se você não sabe a resposta, responda com 'Não sei' e não tente inventar uma resposta.
+        - Se a pergunta  não está relacionada ao jogo, responda com 'Essa pergunta não está relacionada ao jogo'
+        - Considere a data atual ${new Date().toLocaleDateString()}
+        - Faça pesquisas atualizadas sobre o patch atual, baseado na data atual, para dar uma resposta coerente.
+        - Nunca responda itens que você não tenha certeza de que existe no patch atual.
+
+        ## Resposta
+        - Economize na resposta, seja direto e responda no máximo de 500 caracteres. 
+        - Responda em markdown
+        - Não precisa fazer nenhuma saudação ou despedida, apenas responda o que o usuário está querendo.
+
+        ## Exemplo de resposta
+        Pergunda do usuário: Melhor build renger jungle
+        Resposta: A build mais atual é: \n\n **Itens** \n\n Coloque os itens aqui.\n\n **Runas** \n\n Exemplo de runas.\n\n
+
+        ------
+        Aqui está apergunta do usuário: ${question}
+    `
+    const contents = [{
+        role: "user",
+        parts: [{
+            text: pergunta
+        }]
+    }]
+
+    const tools = [{
+        google_search: {}
+    }]
+
+    // chamada API
+    const response = await fetch(geminiURL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'aplication/json'
+        },
+        body: JSON.stringify({
+            contents,
+            tools
+        })
+    })
+
+    const data = await response.json()
+    console.log({data})
+    return data.candidates[0].content.parts[0].text
+}
+
+const enviarFormulario = async (event) => {
     event.preventDefault()
-    console.log(event)
+    const apiKey = apiKeyInput.value 
+    const game = gameSelect.value
+    const question = questionInput.value
+
+    if (apiKey == '' || game == '' || question == '') {
+        alert('Por favor, preencha todos os campos')
+        return
+    }
+
+    askButton.disabled = true
+    askButton.textContent = 'Perguntando...'
+    askButton.classList.add('loading')
+
+    try{
+        //perguntar para a IA
+        const text = await perguntarAI(question, game, apiKey)
+        aiResponse.querySelector('.response-content').innerHTML = markdownToHTML(text)
+        aiResponse.classList.remove('hidden')
+    }catch(error){
+        console.log('Error: ', error)
+    }finally{
+        askButton.desabled = false
+        askButton.textContent = "Perguntar"
+        askButton.classList.remove('loading')
+    }
+
 }
 form.addEventListener('submit', enviarFormulario)
